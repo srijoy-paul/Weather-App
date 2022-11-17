6a434521be3ee8de7fd9f61c878644f46b1400c0
Srijoy Paul <srijoykumar1507@gmail.com>
2022-11-17 23:47:17 +0530

    update daywise forecast
    Signed-off-by: Srijoy Paul <srijoykumar1507@gmail.com>

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const API_key = "76cbcdfffbc0360b58e49191bb4ee752";
 
+const DAY_OF_THE_WEEK = ["sun", "mon", "tue", "wed", "thu", "fri", "sat"];
+
 function createIconURL(iconValue) {
   const iconURL = `http://openweathermap.org/img/wn/${iconValue}@2x.png`;
   // console.log(iconURL);
@@ -44,16 +46,33 @@ async function getHourlyWeatherData(city_name) {
   return data;
 }
 
-function loadHourlyWeatherData(data) {
+function loadHourlyWeatherData(currentWeatherData, data) {
   let innerhtmlText = "";
 
-  for (let hours in data.list) {
+  const timeFormatter = Intl.DateTimeFormat("en", {
+    hour12: true,
+    hour: "numeric",
+  });
+  let dataFor12Hours = data.list.slice(2, 14);
+  console.log(dataFor12Hours);
+
+  innerhtmlText += `<article>
+  <h3 class="time">Now</h3>
+  <img class="icon" src="${createIconURL(
+    currentWeatherData.weather[0].icon
+  )}" alt="" />
+  <p class="hourly-temp">${currentWeatherData.main.temp.toFixed(1)}&#176; C</p>
+</article>`;
+
+  for (let hours in dataFor12Hours) {
     innerhtmlText += `<article>
-      <h3 class="time">${data.list[hours].dt_txt.split(" ")[1]}</h3>
+      <h3 class="time">${timeFormatter.format(
+        new Date(dataFor12Hours[hours].dt_txt)
+      )}</h3>
       <img class="icon" src="${createIconURL(
-        data.list[hours].weather[0].icon
+        dataFor12Hours[hours].weather[0].icon
       )}" alt="" />
-      <p class="hourly-temp">${data.list[hours].main.temp.toFixed(
+      <p class="hourly-temp">${dataFor12Hours[hours].main.temp.toFixed(
         1
       )}&#176; C</p>
     </article>`;
@@ -75,12 +94,65 @@ function loadHumidityData(data) {
   )[0].innerText = `${data.main.humidity}%`;
 }
 
+function calculateDayWiseForecast(data) {
+  let dayWiseForecast = new Map();
+  for (let forecast of data.list) {
+    let day_of_week =
+      DAY_OF_THE_WEEK[new Date(forecast.dt_txt.split(" ")[0]).getDay()];
+
+    if (dayWiseForecast.has(day_of_week)) {
+      let foreCastFortheDay = dayWiseForecast.get(day_of_week);
+      // console.log(foreCastFortheDay);
+      foreCastFortheDay.push(forecast);
+      dayWiseForecast.set(day_of_week, foreCastFortheDay);
+    } else {
+      dayWiseForecast.set(day_of_week, [forecast]);
+    }
+  }
+  console.log(dayWiseForecast);
+  return dayWiseForecast;
+}
+
+function loadFiveDaysData(hourlyData) {
+  const dayWiseForecast = calculateDayWiseForecast(hourlyData);
+  console.log(Array.from(dayWiseForecast.keys()).length);
+  document.getElementsByClassName("days-forecast-title")[0].innerText =
+    Array.from(dayWiseForecast.keys()).length + " day forecast";
+  let innerhtmlText = "";
+  for (let day of dayWiseForecast.keys()) {
+    // console.log(day);
+    let dayWiseData = dayWiseForecast.get(day);
+    // console.log(dayWiseData[0].main.temp_min);
+    let min_temp_day = dayWiseData[0].main.temp_min;
+    let max_temp_day = dayWiseData[0].main.temp_max;
+
+    for (let i = 1; i < dayWiseData.length; i++) {
+      if (min_temp_day > dayWiseData[i].main.temp_min)
+        min_temp_day = dayWiseData[i].main.temp_min;
+
+      if (max_temp_day < dayWiseData[i].main.temp_max)
+        max_temp_day = dayWiseData[i].main.temp_max;
+    }
+    // console.log("Min temp for " + day + " is " + min_temp_day);
+    // console.log("Max temp for " + day + " is " + max_temp_day);
+    innerhtmlText += `<article class="day-forecast">
+    <h3>${day == Array.from(dayWiseForecast.keys())[0] ? "Today" : day}</h3>
+    <img src="${createIconURL(dayWiseData[0].weather[0].icon)}"</p>
+    <p class="min-temp">${min_temp_day.toFixed(1)}&#176; C</p>
+    <p class="max-temp">${max_temp_day.toFixed(1)}&#176; C</p>
+  </article>`;
+  }
+  document.getElementsByClassName("five-days-container")[0].innerHTML =
+    innerhtmlText;
+}
+
 document.addEventListener("DOMContentLoaded", async () => {
   let city = "Dhanbad"; //prompt("Add city")
   const data = await getWeatherData(city);
   const hourlyData = await getHourlyWeatherData(city);
   loadWeatherData(data, hourlyData);
-  loadHourlyWeatherData(hourlyData);
+  loadHourlyWeatherData(data, hourlyData);
   loadFeelsLikeData(data);
   loadHumidityData(data);
+  loadFiveDaysData(hourlyData);
 });
